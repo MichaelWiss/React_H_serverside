a40df4032dd4abbfcc439c1b07c47470cf8e3f82
MichaelWiss <michael.wiss@gmail.com>
2017-01-04 14:31:54 -0600

    redux thunk

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+
+
+export function signinUser({ email, password }) {
+	//submit email/password to server
+	//update state
+	//save JWT token
+	//redirect to route '/feature'
+
+	//if request is badd...
+	//show error to the user
+}
