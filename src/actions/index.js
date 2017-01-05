00a1813412c25ad4f08ff03de3c4f1e826bea5b6
MichaelWiss <michael.wiss@gmail.com>
2017-01-04 18:37:25 -0600

    progrommatic routing

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,22 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+
+
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
 	return function(dispatch) {
 	//submit email/password to server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-
-	//update state
-	//save JWT token
-	//redirect to route '/feature'
-     
-	//if request is badd...
-	//show error to the user
-
-	}
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+         //update state
+	     //save JWT token
+	     //redirect to route '/feature'
+	     browserHistory.push('/feature');
+       })
+      .catch(() => {
+      	//if request is badd...
+	   //show error to the user
+      });
+   }
 }
